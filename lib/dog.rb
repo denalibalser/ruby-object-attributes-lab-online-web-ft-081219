cf07a7fb945d8d2535c4291fdc1e355255bb581e
Denali Balser <denali.balser167@gmail.com>
2019-08-20 14:30:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,10 @@
 class Dog
-  def name=(name)
+  attr_accessor :name, :breed
+
+  def initialize(name, breed)
     @name = name
-  end
-
-  def name
-    @name
-  end
-
-  def breed=(breed)
     @breed = breed
   end
-
-  def breed
-    @breed
-  end
-
+  
 
 end
